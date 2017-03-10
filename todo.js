ecdb644204a71e4c905ab9f35fe4fc576570343b
Maury Gomez <mgomez.sp@mysummitps.org>
2017-03-10 19:50:10 +0000

    final update int3

--- todo.js
+++ todo.js
@@ -1,13 +1,36 @@
-//Varibles for input box, the button and div display
-var inp=document.getElementById("inp");
-var btn=document.getElementById("btn");
-var disp=document.getElementById("disp");
-//button event listener,click
-btn.addEventListener("click",function(){
-  disp.innerHTML+="<div>"+ inp.value+"</div>"
+var todo = document.getElementById("todo");
+var list = document.getElementById("list");
+var disp = document.getElementById("disp");
+var color = document.getElementById("color")
+var clearBtn = document.getElementById("clearBtn")
 
+
+list.addEventListener("click", function() {
+
+  if (color.value === "pink") {
+    disp.innerHTML += "<div style='color:pink'>" + todo.value + "</div>"
+
+  }
+  else if (color.value === "red") {
+    disp.innerHTML += "<div style='color:red'>" + todo.value + "</div>"
+
+  }
+  else if (color.value === "black") {
+    disp.innerHTML += "<div style='color:black'>" + todo.value + "</div>"
+
+  }
+
+  else if (color.value === "green") {
+    disp.innerHTML += "<div style='color:green'>" + todo.value + "</div>"
+
+  }
 });
-//remove item from list
-disp.addEventListener("click", function(evt){
-    evt.target.parentNode.removeChild(evt.target);
+
+disp.addEventListener("click", function(evt) {
+  evt.target.parentNode.removeChild(evt.target);
+})
+
+
+clearBtn.addEventListener("click", function() {
+  disp.innerHTML = ""
 })
